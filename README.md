d115572950c16666f0d62b139013a9175e296000
Toshifumi Kurosawa <kuromailserver@gmail.com>
2024-11-08 22:13:53 +0900

    d

--- README.md
+++ README.md
@@ -9,3 +9,7 @@ $env:APP_ENV="development"; go run ./cmd/main.go
 ```
 swag init -g cmd/main.go
 ``` 
+
+```
+air
+```
--- cmd/main.go
+++ cmd/main.go
@@ -7,55 +7,33 @@ import (
 	"go-echo-gorm/internal/config"
 	"go-echo-gorm/internal/database"
 
+	_ "go-echo-gorm/docs" // swagger docs
+
 	"github.com/labstack/echo/v4"
+	echoSwagger "github.com/swaggo/echo-swagger"
 	"gorm.io/gorm"
 )
 
-// Micropost モデルの定義
+// @title Micropost API
+// @version 1.0
+// @description This is a sample micropost server.
+// @host localhost:8080
+// @BasePath /
 type Micropost struct {
 	ID    uint   `json:"id" gorm:"primaryKey"`
 	Title string `json:"title"`
 }
 
-func main() {
-	// 設定の読み込み
-	cfg, err := config.Load()
-	if err != nil {
-		log.Fatal("Failed to load config:", err)
-	}
-
-	// DBの初期化
-	db, err := database.InitDB(cfg)
-	if err != nil {
-		log.Fatal("Failed to connect to database:", err)
-	}
-
-	// マイグレーション
-	db.AutoMigrate(&Micropost{})
-
-	e := echo.New()
-
-	// CRUD ハンドラーの登録
-	e.POST("/microposts", func(c echo.Context) error {
-		return createMicropost(c, db)
-	})
-	e.GET("/microposts", func(c echo.Context) error {
-		return getMicroposts(c, db)
-	})
-	e.GET("/microposts/:id", func(c echo.Context) error {
-		return getMicropost(c, db)
-	})
-	e.PUT("/microposts/:id", func(c echo.Context) error {
-		return updateMicropost(c, db)
-	})
-	e.DELETE("/microposts/:id", func(c echo.Context) error {
-		return deleteMicropost(c, db)
-	})
-
-	e.Logger.Fatal(e.Start(":8080"))
-}
-
-// Create
+// @Summary Create a new micropost
+// @Description Create a new micropost with the provided title
+// @Tags microposts
+// @Accept json
+// @Produce json
+// @Param micropost body Micropost true "Micropost object"
+// @Success 201 {object} Micropost
+// @Failure 400 {object} map[string]string
+// @Failure 500 {object} map[string]string
+// @Router /microposts [post]
 func createMicropost(c echo.Context, db *gorm.DB) error {
 	micropost := new(Micropost)
 	if err := c.Bind(micropost); err != nil {
@@ -69,7 +47,13 @@ func createMicropost(c echo.Context, db *gorm.DB) error {
 	return c.JSON(http.StatusCreated, micropost)
 }
 
-// Read (List)
+// @Summary Get all microposts
+// @Description Get a list of all microposts
+// @Tags microposts
+// @Produce json
+// @Success 200 {array} Micropost
+// @Failure 500 {object} map[string]string
+// @Router /microposts [get]
 func getMicroposts(c echo.Context, db *gorm.DB) error {
 	var microposts []Micropost
 	if err := db.Find(&microposts).Error; err != nil {
@@ -78,7 +62,14 @@ func getMicroposts(c echo.Context, db *gorm.DB) error {
 	return c.JSON(http.StatusOK, microposts)
 }
 
-// Read (Single)
+// @Summary Get a micropost by ID
+// @Description Get a micropost by its ID
+// @Tags microposts
+// @Produce json
+// @Param id path int true "Micropost ID"
+// @Success 200 {object} Micropost
+// @Failure 404 {object} map[string]string
+// @Router /microposts/{id} [get]
 func getMicropost(c echo.Context, db *gorm.DB) error {
 	id := c.Param("id")
 	micropost := new(Micropost)
@@ -90,7 +81,18 @@ func getMicropost(c echo.Context, db *gorm.DB) error {
 	return c.JSON(http.StatusOK, micropost)
 }
 
-// Update
+// @Summary Update a micropost
+// @Description Update a micropost with the provided title
+// @Tags microposts
+// @Accept json
+// @Produce json
+// @Param id path int true "Micropost ID"
+// @Param micropost body Micropost true "Micropost object"
+// @Success 200 {object} Micropost
+// @Failure 400 {object} map[string]string
+// @Failure 404 {object} map[string]string
+// @Failure 500 {object} map[string]string
+// @Router /microposts/{id} [put]
 func updateMicropost(c echo.Context, db *gorm.DB) error {
 	id := c.Param("id")
 	micropost := new(Micropost)
@@ -110,7 +112,13 @@ func updateMicropost(c echo.Context, db *gorm.DB) error {
 	return c.JSON(http.StatusOK, micropost)
 }
 
-// Delete
+// @Summary Delete a micropost
+// @Description Delete a micropost by its ID
+// @Tags microposts
+// @Param id path int true "Micropost ID"
+// @Success 204 "No Content"
+// @Failure 500 {object} map[string]string
+// @Router /microposts/{id} [delete]
 func deleteMicropost(c echo.Context, db *gorm.DB) error {
 	id := c.Param("id")
 
@@ -119,4 +127,45 @@ func deleteMicropost(c echo.Context, db *gorm.DB) error {
 	}
 
 	return c.NoContent(http.StatusNoContent)
+}
+
+func main() {
+	// 設定の読み込み
+	cfg, err := config.Load()
+	if err != nil {
+		log.Fatal("Failed to load config:", err)
+	}
+
+	// DBの初期化
+	db, err := database.InitDB(cfg)
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	}
+
+	// マイグレ��ション
+	db.AutoMigrate(&Micropost{})
+
+	e := echo.New()
+
+	// Swagger UIのエンドポイントを追加
+	e.GET("/swagger/*", echoSwagger.WrapHandler)
+
+	// CRUD ハンドラーの登録
+	e.POST("/microposts", func(c echo.Context) error {
+		return createMicropost(c, db)
+	})
+	e.GET("/microposts", func(c echo.Context) error {
+		return getMicroposts(c, db)
+	})
+	e.GET("/microposts/:id", func(c echo.Context) error {
+		return getMicropost(c, db)
+	})
+	e.PUT("/microposts/:id", func(c echo.Context) error {
+		return updateMicropost(c, db)
+	})
+	e.DELETE("/microposts/:id", func(c echo.Context) error {
+		return deleteMicropost(c, db)
+	})
+
+	e.Logger.Fatal(e.Start(":8080"))
 }
