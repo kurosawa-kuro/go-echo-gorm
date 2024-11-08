b5828c3f380b95d37a95e78c495555e5f8865c49
Toshifumi Kurosawa <kuromailserver@gmail.com>
2024-11-08 22:01:00 +0900

    ｗ

--- cmd/main.go
+++ cmd/main.go
@@ -2,15 +2,31 @@ package main
 
 import (
 	"log"
+	"net/http"
 
 	"go-echo-gorm/internal/config"
+
+	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	// 設定の読み込み
-	cfg, err := config.Load()
+	cfd, err := config.Load()
 	if err != nil {
 		log.Fatal("Failed to load config:", err)
 	}
-	log.Println("Config loaded:", cfg)
+	log.Println("Config loaded:", cfd)
+
+	// Echoインスタンスの作成
+	e := echo.New()
+
+	// ルートの設定
+	e.GET("/ping", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, map[string]string{
+			"message": "pong",
+		})
+	})
+
+	// サーバーの起動
+	e.Logger.Fatal(e.Start(":8080"))
 }
